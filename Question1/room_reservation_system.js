eeae36bde336fdf209fb6a85e330c5348b040aa9
fana <fanabezabih@gmail.com>
2025-06-02 14:07:40 +0300

    added answers for building a room reservation in js

---
+++ Question1/room_reservation_system.js
@@ -0,0 +1,20 @@
+
+function reservation(roomNumber,guestDetails){
+    
+    const reservedRooms=[20,30,40,12];
+    for(room in reservedRooms){
+      
+        if(room!=roomNumber){
+            const bookRoom={}
+             bookRoom[roomNumber]= guestDetails;
+        return bookRoom
+       
+        }
+        else{
+            console.log("The room is taken")
+        }
+       
+    }
+}
+const reserve=reservation(20,[{name:"Alia",checkInDetails:"02-03-05"}])
+console.log(reserve) 
